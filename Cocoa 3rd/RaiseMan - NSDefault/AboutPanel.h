fbaad6431ba840c332ae7dad9f069a4fa90de32b
kkabdol <kkabdol@c6564fb8-f78f-11de-abf5-6d7912ff0902>
2010-01-13 07:01:45 +0000

    ch13

---
+++ Cocoa 3rd/RaiseMan - NSDefault/AboutPanel.h
@@ -0,0 +1,14 @@
+//
+//  AboutPanel.h
+//  RaiseMan
+//
+//  Created by Seonghyeon Choe on 1/12/10.
+//  Copyright 2010 kkabdol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AboutPanel : NSWindowController {
+
+}
+@end
